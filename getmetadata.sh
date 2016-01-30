f02134d3c242ee464b3f86dda01bb46ad909c3c8
Shamil Turner <shamil.turner@gmail.com>
2016-01-30 08:59:52 -0500

    Created variable and parameter to be passed to populate PATH.

--- getmetadata.sh
+++ getmetadata.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 #Get the metadata from the dropbox api for a file
+# ./getmetadata.sh <path>
 
+OBJECT_PATH=$1
 
-curl https://api.dropboxapi.com/1/metadata/auto/ -H "Authorization:Bearer $DROPBOX_ACCESS_TOKEN"
+curl https://api.dropboxapi.com/1/metadata/auto$OBJECT_PATH -H "Authorization:Bearer $DROPBOX_ACCESS_TOKEN"
