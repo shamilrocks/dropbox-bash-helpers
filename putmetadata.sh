435e886759ddf0fc6188d64bd9c7e00fc3628801
Shamil Turner <shamil.turner@gmail.com>
2016-01-30 10:24:16 -0500

    Getting Metadata from Dropbox directory and creating file in Dropbox.

--- putmetadata.sh
+++ putmetadata.sh
@@ -2,14 +2,17 @@
 # ./putmetadata <intialpath>
 OBJECT_PATH=$1
 TEMPMETADATA="tempmetadata.json"
+
 #get the metadata by providing path and calling getmetadata function
+#take output of getmetadata and put in file called, tempmetadata
+
 echo "Getting Metadata from DropBox!!!!"
 ./getmetadata.sh $OBJECT_PATH > $TEMPMETADATA
 echo "File $TEMPMETADATA Created!!!!"
-#take output of getmetadata and put in file called, tempmetadata
-
 #put the object into new path, by calling putobject providing parameter 1 being tempmetadata, parameter 2 being updated from initial path
-
+echo "Pushing to Dropbox!!!"
+./putobject.sh $TEMPMETADATA "$OBJECT_PATH-$(date +%s)"
 #confirm name by echo out to console then delete tempmetadata file
+
 echo "Cleaning Up!"
 rm $TEMPMETADATA
